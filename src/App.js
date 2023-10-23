93e79672f23011f6908f800917e6cc2c2da7032c
unknown <jjmayhew2@gmail.com>
2023-10-23 22:42:03 +0800

    initialize commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+import { Navbar } from './Components/Navbar/Navbar';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { ShopCategory } from './Pages/ShopCategory';
+import { LoginSignup } from './Pages/LoginSignup';
+import { Cart } from './Pages/Cart';
+import { Shop } from './Pages/Shop';
+import { Product } from './Pages/Product';
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+     <Navbar/>
+    <Routes>
+      <Route path='/' element ={<Shop/>}/>
+      <Route path='/men' element ={<ShopCategory category ="men"/>}/>
+      <Route path='/women' element ={<ShopCategory category ="women"/>}/>
+      <Route path='/kids' element ={<ShopCategory category ="kids"/>}/>
+      <Route path='/' element ={<Shop/>}/>
+      <Route path="product" element ={<Product/>}>
+        <Route path=':productId' element={<Product/>}/>
+      </Route>
+      <Route path='/cart' element ={<Cart/>}/>
+      <Route path='/login' element ={<LoginSignup/>}/>
+ 
+
+
+    </Routes>
+
+
+
+     </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
